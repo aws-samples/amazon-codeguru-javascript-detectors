2b5a17521cd11dd1dac156160c1bb48cf396c56a
Vidhi Khandelwal <rvidhikh@amazon.com>
2023-02-24 11:05:54 +0530

    git commit -m "Add compliant and noncompliant examples of javascript//file-injection@v1.0"

---
+++ src/javascript/detectors/file_injection/file_injection.js
@@ -0,0 +1,30 @@
+// {fact rule=file-injection@v1.0 defects=1}
+var fs = require('fs')
+var express = require('express')
+var app = express()
+function fileInjectionNoncompliant() {
+    app.get('www.example.com', (req,res) => {
+        var data = req.params.data
+            // Noncompliant: writing unsanitized user data to a file.
+            fs.writeFile('data.txt', data, function(err){
+                if(err) throw err
+            })
+    })
+}
+// {/fact}
+
+
+// {fact rule=file-injection@v1.0 defects=0}
+var fs = require('fs')
+var express = require('express')
+var app = express()
+function fileInjectionCompliant() {
+    app.get('www.example.com', (req,res) => {
+        var data = sanitize(req.params.data)
+        // Compliant: user input is sanitized before use.
+        fs.writeFile('data.txt', data, function(err){
+            if(err) throw err
+        })
+    })
+}
+// {/fact}
