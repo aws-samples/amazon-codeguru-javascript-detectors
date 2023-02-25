de4f19fdd90820c76783c2788b5791ceeafd16c8
Swaraj Gore <swarajgo@amazon.com>
2023-02-25 18:03:27 +0530

    Change compliant and noncompliant examples of hardcoded-credentials@v1.0

--- src/javascript/detectors/hardcoded_credentials/hardcoded_credentials.js
+++ src/javascript/detectors/hardcoded_credentials/hardcoded_credentials.js
@@ -4,26 +4,28 @@
  */
 
 // {fact rule=hardcoded-credentials@v1.0 defects=1}
-var jose = require("jose")
-var { JWT } = jose
-function hardcodedCredentialsNoncompliant() {
-    let token = JWT.sign({
-        username: 'username',
-        // Noncompliant: password is hardcoded.
-        password : 'password'
+var crypto = require('crypto')
+function hardcodedCredentialsNoncompliant(salt, iterations, keyLen, digest) {
+    // Noncompliant: password is hardcoded.
+    crypto.pbkdf2('password', salt, iterations, keyLen, digest, (err, key) => {
+        if (err) {
+            throw err
+        }
+        return key.toString('base64')
     })
 }
 // {/fact}
 
 
 // {fact rule=hardcoded-credentials@v1.0 defects=0}
-var jose = require("jose")
-var { JWT } = jose
-function hardcodedCredentialsCompliant() {
-    let token = JWT.sign({
-        username: 'username',
-        // Compliant: password is obtained from environment.
-        password: process.env.PASSWORD
+var crypto = require('crypto')
+function hardcodedCredentialsCompliant(salt, iterations, keyLen, digest) {
+    // Compliant: password is obtained from environment.
+    crypto.pbkdf2(process.env.password, salt, iterations, keyLen, digest, (err, key) => {
+        if (err) {
+            throw err
+        }
+        return key.toString('base64')
     })
 }
 // {/fact}
