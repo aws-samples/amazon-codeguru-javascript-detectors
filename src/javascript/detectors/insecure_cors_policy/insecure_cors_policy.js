6e059b7f8b211550942c756d302b91ad26871dce
Swaraj Gore <swarajgo@amazon.com>
2023-02-28 16:29:34 +0530

    Add header keyword in comments and Update GitHub URLs in package.json

--- src/javascript/detectors/insecure_cors_policy/insecure_cors_policy.js
+++ src/javascript/detectors/insecure_cors_policy/insecure_cors_policy.js
@@ -9,7 +9,7 @@ var app = express()
 function insecureCorsPolicyNoncompliant() {
     app.post('/users', function (req, res) {
         const origin = req.query.origin
-        // Noncompliant : the Access-Control-Allow-Origin is set to user-controlled any domain.
+        // Noncompliant : the Access-Control-Allow-Origin header is set to user-controlled any domain.
         res.set(200, {'Access-Control-Allow-Origin': origin })
     })
 }
@@ -21,7 +21,7 @@ var express = require("express")
 var app = express()
 function insecureCorsPolicyCompliant() {
     app.post('/users', function (req, res) {
-        // Compliant: the Access-Control-Allow-Origin is set to allow only a specific list of trusted domains.
+        // Compliant: the Access-Control-Allow-Origin header is set to allow only a specific list of trusted domains.
         res.set(200, {'Access-Control-Allow-Origin': 'trustedsite.com' })
     })
 }
