6433943669033c2330e3712c1cf400f203ea4793
kamlesh gour <ngoukaml@amazon.com>
2023-03-01 09:51:57 +0530

    Changed what was suggested of javascript/insecure-temp-file@v1.0

--- src/detectors/insecure_temporary_file/insecure_temporary_file.js
+++ src/detectors/insecure_temporary_file/insecure_temporary_file.js
@@ -6,7 +6,7 @@
 // {fact rule=insecure-temp-file@v1.0 defects=1}
 var fs = require('fs')
 function insecureTempFileNoncompliant() {
-    // Noncompliant: the global directory path is given for opening a file or creating a file the case can be vulnerable to injection attacks.
+    // Noncompliant: the global directory path is given for opening a file or creating a file which can be vulnerable to injection attacks.
     var tmp_file = "/tmp/f"
     fs.readFile(tmp_file, 'utf8', function (err, data) {
        // ...
@@ -18,7 +18,7 @@ function insecureTempFileNoncompliant() {
 var fs = require('fs')
 var tmp = require('tmp')
 function insecureTempFileCompliant() {
-    // Compliant: tmp library to securely create or read a temporary files.
+    // Compliant: tmp library to securely create or read temporary files.
    var tmp_obj = tmp.fileSync()
    fs.readFile(tmp_obj, 'utf8')
 }
