0f193567c8a69070fec716111f5ee70483e02e45
Ankit Patil <ankipatd@amazon.com>
2023-03-02 15:31:49 +0530

    change noncompliant and compliant examples for javascript/integer-overflow@v1.0

--- src/javascript/detectors/integer_overflow/integer_overflow.js
+++ src/javascript/detectors/integer_overflow/integer_overflow.js
@@ -5,15 +5,15 @@
 
 // {fact rule=integer-overflow@v1.0 defects=1}
 function integerOverflowNoncompliant() {
-    // Noncompliant: big int is assigned to a variable.
-    var max = 1e21
+    // Noncompliant: bigint is assigned to a variable.
+    var max = 154327115334273650000012748329
 }
 // {/fact}
 
 
 // {fact rule=integer-overflow@v1.0 defects=0}
 function integerOverflowCompliant() {
-    // Compliant: value assigned to a variable is not big int.
-    var max = 1234
+    // Compliant: n is appended to an integer which makes it a bigint.
+    var max = 154327115334273650000012748329n
 }
 // {/fact}
