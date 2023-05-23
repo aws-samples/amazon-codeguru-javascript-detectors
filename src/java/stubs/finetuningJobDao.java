19bcadf70dbb7b415ca667c25c21477fc32eb6a6
Vladyslav Boreiko <Vladyslav.Boreiko@softengi.com>
2023-05-23 22:47:49 +0300

    try java

---
+++ src/java/stubs/finetuningJobDao.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package stubs;
+
+public class finetuningJobDao {
+    public static ActivityJobItem loadFinetuningJob(String activityJobArn) {
+        return null;
+    }
+}
